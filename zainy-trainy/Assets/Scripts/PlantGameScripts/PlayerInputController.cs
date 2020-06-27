3d41c49137fbbf1818186b378790f46c868b97f3
Thomas Storwick <tstorwick@gmail.com>
2020-06-27 13:50:17 -0400

    inputs somewhat work

---
+++ zainy-trainy/Assets/Scripts/PlantGameScripts/PlayerInputController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private IAmAMainTrainPlayer player;
+
+    public PlantPlayerInputs currentInputs;
+
+
+    void Awake()
+    {
+        currentInputs = GetComponent<PlayerPlatformerController>().currentInputs;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnWKey(InputAction.CallbackContext context)
+    {
+        GetComponent<PlayerPlatformerController>().currentInputs.jumping = context.ReadValue<float>() == 1;
+
+    }
+
+    public void OnAKey(InputAction.CallbackContext context)
+    {
+        if (context.ReadValue<float>() == 1)
+        {
+            GetComponent<PlayerPlatformerController>().currentInputs.horizontal = -1;
+        }
+    }
+
+    public void OnSKey(InputAction.CallbackContext context)
+    {
+        GetComponent<PlayerPlatformerController>().currentInputs.down = context.ReadValue<float>() == 1;
+
+    }
+
+    public void OnDKey(InputAction.CallbackContext context)
+    {
+        if (context.ReadValue<float>() == 1)
+        {
+            GetComponent<PlayerPlatformerController>().currentInputs.horizontal = 1;
+        }
+    }
+}
--- zainy-trainy/Assets/Scripts/PlantGameScripts/PlayerPlatformerController.cs
+++ zainy-trainy/Assets/Scripts/PlantGameScripts/PlayerPlatformerController.cs
@@ -1,50 +1,80 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerPlatformerController : PhysicsObject
+public struct PlantPlayerInputs
+{
+    public int horizontal;
+    public bool jumping;
+    public bool down;
+
+}
+
+public class PlayerPlatformerController : MonoBehaviour
 {
 
     public float maxSpeed = 7;
     public float jumpTakeOffSpeed = 7;
+    Vector2 velocity;
+    bool grounded;
 
     private SpriteRenderer spriteRenderer;
     private Animator animator;
+
+    public PlantPlayerInputs currentInputs;
+
+    Rigidbody2D rb2d;
 
     // Use this for initialization
     void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        animator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+        currentInputs.horizontal = 0;
+        currentInputs.jumping = false;
+        currentInputs.down = false;
     }
 
-    protected override void ComputeVelocity()
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 targetVelocity = ComputeVelocity();
+        velocity.x = targetVelocity.x;
+        //velocity += Physics2D.gravity * Time.deltaTime;
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+        rb2d.position = rb2d.position + deltaPosition;
+    }
+
+    Vector2 ComputeVelocity()
     {
         Vector2 move = Vector2.zero;
 
-        move.x = Input.GetAxis("Horizontal");
+        move.x = currentInputs.horizontal;
+        print(currentInputs.horizontal);
 
-        if (Input.GetButtonDown("Jump") && grounded)
+        if (currentInputs.jumping)
         {
             velocity.y = jumpTakeOffSpeed;
         }
-        else if (Input.GetButtonUp("Jump"))
-        {
-            if (velocity.y > 0)
-            {
-                velocity.y = velocity.y * 0.5f;
-            }
-        }
+        //else if (currentInputs.jumping)
+        //{
+        //    if (velocity.y > 0)
+        //    {
+        //        velocity.y = velocity.y * 0.5f;
+        //    }
+        //}
 
-        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
-        if (flipSprite)
-        {
-            spriteRenderer.flipX = !spriteRenderer.flipX;
-        }
+        //bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
+        //if (flipSprite)
+        //{
+        //    spriteRenderer.flipX = !spriteRenderer.flipX;
+        //}
 
-        animator.SetBool("grounded", grounded);
-        animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
-
-        targetVelocity = move * maxSpeed;
+        return move * maxSpeed;
     }
 }
