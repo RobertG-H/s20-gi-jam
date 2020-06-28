1fae696cec7066978d80b4b8a558c0b5c2743ec9
johnnybib <jamesgrahamhu@gmail.com>
2020-06-27 23:52:31 -0600

    Fixing layering on robots

---
+++ zainy-trainy/Assets/Scripts/MainTrain/TrackController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackController : MonoBehaviour
+{
+    public GameObject track1;
+    public GameObject track2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
