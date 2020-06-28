69a544f968852b644c444495ee0e76a7314df62c
johnnybib <jamesgrahamhu@gmail.com>
2020-06-28 00:45:07 -0600

    Added moving tracks

--- zainy-trainy/Assets/Scripts/MainTrain/TrackController.cs
+++ zainy-trainy/Assets/Scripts/MainTrain/TrackController.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 
 public class TrackController : MonoBehaviour
 {
-    public GameObject track1;
-    public GameObject track2;
+    public List<GameObject> tracks;
+    private SpriteRenderer trackSprite;
+    public float speed;
+    private float halfLength;
+    public Transform endPoint;
     // Start is called before the first frame update
     void Start()
     {
-        
+        trackSprite = tracks[0].GetComponent<SpriteRenderer>();
+        halfLength = trackSprite.sprite.bounds.center.x - trackSprite.sprite.bounds.min.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for(int i = 0; i < tracks.Count; i++)
+        {
+            tracks[i].transform.position += speed * Vector3.right * Time.deltaTime;
+            if(tracks[i].transform.position.x - halfLength >= endPoint.position.x)
+            {
+                int idx = i-1;
+                if(idx < 0)
+                {
+                    idx = tracks.Count - 1;
+                }
+                Vector3 newPos = tracks[idx].transform.position;
+                newPos.x = newPos.x - halfLength;
+                tracks[i].transform.position = newPos;
+            }
+        }
     }
 }
